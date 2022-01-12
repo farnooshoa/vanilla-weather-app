911e46468a81a9a6f47947653a2c27e8eb6011dd
farnooshoa <farnooshoa@gmail.com>
2022-01-12 10:29:00 +0330

    update the time

--- venilla-weather-app/src/app.js
+++ venilla-weather-app/src/app.js
@@ -1,3 +1,33 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+}
 function displayTemperature(response) {
     console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
@@ -5,12 +35,13 @@ function displayTemperature(response) {
     let cityElement = document.querySelector("#city");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     discriptionElement.innerHTML = response.data.weather[0].description;
     cityElement.innerHTML = response.data.name;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
-
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = "256a30f9fb5ee55039e6c546147b0e63";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
